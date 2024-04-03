32e13684b66eae689885b66bd4cf0f15ade5b6b5
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-04-03 15:29:36 -0300

    initial commit

---
+++ src/GerenciadorDeProdutos.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorDeProdutos {
+    public List <Produto> produtos = new ArrayList<Produto>();
+    // C
+    public boolean AdcionarProduto (Produto produto){
+        return produtos.add(produto);
+    }
+    // R
+    public List <Produto> ListarProduto(){
+        return produtos;
+    }
+
+    // U
+    public
+}
---
+++ src/Produto.java
@@ -0,0 +1,10 @@
+public class Produto {
+    public int id;
+    public String nome;
+    public double preco;
+    public Produto (int id, String nome, double preco){
+        this.nome = nome;
+        this.id = id;
+        this.preco = preco;
+    }
+}
